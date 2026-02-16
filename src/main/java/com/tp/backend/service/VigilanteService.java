a1ae805f230ff87e2cfe3e28236d3b6a119e6b93
MaEugeniaDev <meugeniabarca@gmail.com>
2026-02-16 03:42:21 -0300

    avances nuevos

--- src/main/java/com/tp/backend/service/VigilanteService.java
+++ src/main/java/com/tp/backend/service/VigilanteService.java
@@ -1,5 +1,7 @@
 package com.tp.backend.service;
 
+//avances nuevos
+
 import com.tp.backend.dto.contrato.ContratoResponse;
 import com.tp.backend.dto.vigilante.*;
 import com.tp.backend.exception.BadRequestException;
